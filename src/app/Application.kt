a19eb356ad1a101efc53da52db6ac4fef9ca6834
Theo Bouwman <theobouwman98@gmail.com>
2020-11-26 18:21:49 +0100

    health route

--- src/app/Application.kt
+++ src/app/Application.kt
@@ -38,6 +38,7 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
+        commonRoutes()
         apiV1Routes()
     }
 }
--- src/app/Routing.kt
+++ src/app/Routing.kt
@@ -1,8 +1,16 @@
 package app
 
 import app.controllers.ExampleController
+import io.ktor.application.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import org.kodein.di.ktor.controller.controller
+
+fun Routing.commonRoutes() {
+    get("/health") {
+        call.respondText("OK")
+    }
+}
 
 fun Routing.apiV1Routes() {
     route("/api/v1") {
